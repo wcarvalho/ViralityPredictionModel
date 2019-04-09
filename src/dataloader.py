75c75518deeffe364c3a5cb5416427f926fda89c
Wilka Carvalho <wcarvalho92@gmail.com>
2019-04-09 09:47:05 -0400

    really basic dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,36 +1,35 @@
+# NOTE: best option seems to be to shuffle the csv outside of python every epoch and then read in chunks at a time
+
 import torch
 from torch.utils.data import Dataset, DataLoader
-# import pandas
+import pandas as pd
 import csv
 import random
 import argparse
 
-class TwitterDataset(Dataset):
-  """docstring for TwitterDataset"""
-  def __init__(self, main_csv_file, colnames, file_length=0):
-    super(TwitterDataset, self).__init__()
-    self.main_csv_file = main_csv_file
-    self.colnames = colnames
+class TwitterDataloader(object):
+  """docstring for TwitterDataloader"""
+  def __init__(self, filename, colnames, file_length, batch_size):
+    super(TwitterDataloader, self).__init__()
+    self.df = pd.read_csv(filename, names=colnames, header=None, chunksize=batch_size)
+    self.batch_size = batch_size
+    self.filename = filename
     if not file_length:
       self.file_length = sum(1 for line in open(self.main_csv_file))
     else:
       self.file_length = file_length
 
-  def __getitem__(self, i):
-    # NOTE: best option seems to be to shuffle the csv outside of python every epoch and then read in chunks at a time
-    # indexing into the csv seems very difficult
-    # presumably, the CSV will be very large and we don't want to read it all into memory.
+  def __iter__(self):
+    self.indx = 0
+    return self
+  
+  def __next__(self):
+    if self.indx < self.file_length:
+      self.indx += self.batch_size
+      return next(self.df)
+    else:
+      raise StopIteration
 
-    # if self.default.data_loader == "regular": return self.regular_get(i)
-    # skip = sorted(random.sample(range(self.file_length),self.file_length-128))
-    # best option 
-    # with open(self.main_csv_file) as f:
-    #   r = csv.DictReader(f)
-    #   import ipdb; ipdb.set_trace()
-    # df = pandas.read_csv(self.main_csv_file, names=colnames, header=None, skiprows=skip)
-
-  def __len__(self): return self.file_length
-    
 if __name__ == '__main__':
   import argparse
 
@@ -38,19 +37,11 @@ if __name__ == '__main__':
   parser.add_argument('-f', '--filename', type=str)
   parser.add_argument('-bs', '--batch-size', type=int, default=128)
   parser.add_argument('-fl', '--file-length', type=int, default=0)
-  parser.add_argument('-s', '--shuffle', type=int, default=1, choices=[0,1])
+  # parser.add_argument('-s', '--shuffle', type=int, default=0, choices=[0,1])
+  parser.add_argument('-cn', '--colnames', type=str, default=["r_pid", "r_uid", "r_t", "p_pid", "p_uid", "p_t", "c_pid", "c_uid", "c_t"], nargs='+')
   args, unknown = parser.parse_known_args()
 
   batch_size = args.batch_size
 
-  # skip = sorted(random.sample(range(file_length),file_length-batch_size))
 
-  colnames = ["r_pid", "r_uid", "r_t", "p_pid", "p_uid", "p_t", "c_pid", "c_uid", "c_t"]
-  
-  dataset = TwitterDataset(args.filename, colnames)
-  dataloader = DataLoader(
-      dataset,
-      batch_size=batch_size,
-      shuffle=args.shuffle,)
-  for x in dataloader:
-    continue
+  dataloader = TwitterDataloader(args.filename, args.colnames, args.file_length, args.batch_size)
