37e64e7bc3255c7c96173e13abda75b8cc85e145
Yunseok Jang <yunseokj@umich.edu>
2019-04-08 22:28:00 -0400

    add BERT based text feature extractor

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # ViralityPredictionModel
 
 ## Input
-### 1. set of rows that contains r_pid, r_uid, r_t, p_pid, p_uid, p_t, c_pid, c_uid, c_t (where r=root, p=parent, c=child, pid=post_id, uid=user_id, t=timestamp)
-### 2. Image array (key,value dictionary, created with hdf5 format):  key=r_pid, value={"vector": image or video_vector, "label": image/video/(gif?)}
-### 3. Text array (key,value dictionary, created with hdf5 format):    key=r_pid, value={"vector": text_vector}
+
+### 1. graph
+set of rows that contains r_pid, r_uid, r_t, p_pid, p_uid, p_t, c_pid, c_uid, c_t (where r=root, p=parent, c=child, pid=post_id, uid=user_id, t=timestamp)
+
+### 2. Image array
+
+(key,value dictionary, created with hdf5 format):  key=r_pid, value={"vector": image or video_vector, "label": image/video/(gif?)}
+
+### 3. Text array
+
+(key,value dictionary, created with hdf5 format):    key=r_pid, value={"vector": text_vector}
+
+- Extracted from BERT
+- Specific model name: bert-base-multilingual-cased
+- dim for each vector: 768
+
---
+++ extractors/text/bert.py
@@ -0,0 +1,102 @@
+from __future__ import print_function
+from __future__ import division
+
+import torch
+from pytorch_pretrained_bert import BertTokenizer, BertModel
+
+# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import sys
+import re
+import csv
+import numpy as np
+import h5py
+from tqdm import trange
+
+match = re.compile(r'(?:\@|https?\://)\S+|\[CLS\]|\[SEP\]|\#')
+
+
+
+def main(argv):
+    if len(argv) not in [2,3]:
+        print('wrong command. It should be python bery.py SPLIT_NO [BATCH_SIZE]')
+        return
+
+    SPLIT_NO = int(argv[1])
+    if len(argv) == 3:
+        BATCH_SIZE = int(argv[2])
+    else:
+        BATCH_SIZE = 512
+
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased',
+                                              do_lower_case=False,
+                                              max_len=128,
+                                              do_basic_tokenize=True,
+                                              cache_dir='./checkpoints/')
+    id_word_list = []
+    with open('../../dataset/orig_text_segment{}'.format(SPLIT_NO)) as f:
+        reader = csv.reader(f, delimiter=',')
+        for target in reader:
+            key = int(target[0])
+            value = ','.join(target[1:])
+
+            # remove url, mention, hashtag, special token used in BERT
+            value = match.sub("", value)
+            # put special char for BERT
+            value = '[CLS] ' + value + ' [SEP]'
+
+            # tokenize
+            tokenized_text = tokenizer.tokenize(value)
+
+           # convert to index
+            indexes = tokenizer.convert_tokens_to_ids(tokenized_text)
+
+            # save it to array
+            id_word_list.append([key, indexes])
+            #if len(id_word_list) > 5000:
+            #    break
+
+    # Load pre-trained model (weights)
+    model = BertModel.from_pretrained('bert-base-multilingual-cased',
+                                      cache_dir='./checkpoints/')
+    model.eval()
+    # If you have a GPU, put everything on cuda
+    model.to('cuda')
+
+    hf = h5py.File('../../dataset/text_features{}.h5'.format(SPLIT_NO), 'w')
+    answer_dict = {}
+    for idx in trange(((len(id_word_list) + BATCH_SIZE - 1) // BATCH_SIZE), ncols=60):
+        batch_list = id_word_list[idx * BATCH_SIZE:(idx + 1) * BATCH_SIZE]
+        if len(batch_list) == 0:
+            continue
+
+        # group each of them
+        id_list = [_[0] for _ in batch_list]
+        indexed_tokens = [_[1] for _ in batch_list]
+
+        tokens_tensor = torch.nn.utils.rnn.pad_sequence([torch.tensor(_) for _ in indexed_tokens], batch_first=True)
+        # If you have a GPU, put everything on cuda
+        tokens_tensor = tokens_tensor.to('cuda')
+        segments_tensors = torch.zeros_like(tokens_tensor)
+
+        # Predict hidden states features for each layer
+        with torch.no_grad():
+            _, pooled_output = model(input_ids=tokens_tensor,
+                                     token_type_ids=segments_tensors,
+                                     output_all_encoded_layers=False)
+
+            # pooled_output: [batch_size, hidden_size (768)]
+        value_list = pooled_output.data.cpu().numpy()
+        for each_key, each_value in zip(id_list, value_list):
+            grp = hf.create_group(str(each_key))
+            grp.create_dataset('vector', data=each_value)
+    hf.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
