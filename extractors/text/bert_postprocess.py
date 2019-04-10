7b9074c4281e80f109b4f0082a0c415329802fdb
Yunseok Jang <yunseokj@umich.edu>
2019-04-10 09:55:47 -0400

    change into bucketing

--- extractors/text/bert_postprocess.py
+++ extractors/text/bert_postprocess.py
@@ -8,13 +8,13 @@ from tqdm import trange, tqdm
 import h5py
 import glob
 import numpy as np
-import pickle
+import operator
 
-BUCKET_BASE = 2000
+BUCKET_BASE = 300000
 
 def main(argv):
     word_feature_dict = {}
-    write_dir = '/mnt/brain4/datasets/Twitter/final/'
+    write_dir = '/mnt/brain4/datasets/Twitter/final/text'
     for split_no in range(4):
         base_dir = '../../dataset/bert_text_output{}'.format(split_no)
         list_of_items = glob.glob(base_dir+'/*.npz')
@@ -25,26 +25,19 @@ def main(argv):
             npz_data = np.load(os.path.join(base_dir, '{:06d}.npz'.format(file_id)))
             for pid, value in zip(npz_data['pid'], npz_data['value']):
                 word_feature_dict[pid] = value
+    # sort first
+    sorted_list = sorted(word_feature_dict.items(), key=operator.itemgetter(0))
 
-    # first save the large chunk
-    with open(os.path.join(write_dir, 'user_text.pickle'), 'wb') as f:
-        pickle.dump(word_feature_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-    #keys = np.array(word_feature_dict.keys())
-    #min_id = np.amin(keys)
-    #max_id = np.amax(keys)
-    #
-    #bucket_min = min_id // BUCKET_BASE
-    #bucket_max = max_id // BUCKET_BASE
-    #
-    #for bucket_id in range(bucket_min, bucket_max+1):
-    #    hf = h5py.File('/mnt/brain4/datasets/Twitter/user_text_{}.h5'.format(bucket_id * BUCKET_BASE), 'w')
-    #    for offset in range(BUCKET_BASE):
-    #        pid = bucket_id * BUCKET_BASE + offset
-    #        if pid in word_feature_dict:
-    #            grp = hf.create_group(str(pid))
-    #            grp.create_dataset('text', data=word_feature_dict[pid])
-    #    hf.flush()
+    # save output by bucket
+    for idx in trange((len(sorted_list) + BUCKET_BASE - 1) // BUCKET_BASE, ncols=60):
+        batch_list = sorted_list[idx * BUCKET_BASE:(idx+1) * BUCKET_BASE]
+        if len(batch_list) == 0:
+            continue
+        hf = h5py.File(os.path.join(write_dir, '{}_{}.h5'.format(batch_list[0][0], batch_list[-1][0])), 'w')
+        for pid, vlaue in batch_list:
+            grp = hf.create_group(str(pid))
+            grp.create_dataset('text', data=value)
+        hf.flush()
 
 
 if __name__ == "__main__":
